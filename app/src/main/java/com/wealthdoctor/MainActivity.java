701fa7d0186418e722d0e0b23a2b3229a9e0231f
Girish B N <girishbn4@gmail.com>
2017-12-19 15:37:56 +0530

    Testing

--- app/src/main/java/com/wealthdoctor/MainActivity.java
+++ app/src/main/java/com/wealthdoctor/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("MainActivity","Girish");
+        Log.i("MainActivity","Girish 1");
     }
 }
